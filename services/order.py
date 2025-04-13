8887de8da2eec2e998f43ab54ac4937cbd89361e
Max <smolinskijmaksim1@gmail.com>
2025-04-13 20:45:30 +0100

    Solution

--- services/order.py
+++ services/order.py
@@ -1,15 +1,17 @@
 from django.contrib.auth import get_user_model
+from django.db import transaction
 
 from db.models import Order, Ticket, MovieSession
 from datetime import datetime
+import QuerySet
 
 
 def create_order(
         tickets: list[dict],
         username: str, date:
         datetime = None) -> Order:
-    User = get_user_model()
-    user, _ = User.objects.get_or_create(username=username)
+    user = get_user_model()
+    user, _ = user.objects.get_or_create(username=username)
 
     order = Order.objects.create(user=user)
     if date:
@@ -17,9 +19,16 @@ def create_order(
         order.save()
     for ticket in tickets:
         Ticket.objects.create(
-            row=ticket['row'],
-            seat=ticket['seat'],
-            movie_session=MovieSession.objects.get(id=ticket['movie_session']),
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session=MovieSession.objects.get(id=ticket["movie_session"]),
             order=order
         )
     return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order, Order] | None:
+    with transaction.atomic():
+        if username:
+            return Order.objects.filter(user__username=username)
+        return Order.objects.all()
